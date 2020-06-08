f52d4b40c766ad8fee811f4af7ee9b3b3a560531
Maher Bouidani <maher.buedani@gmail.com>
2020-06-08 19:02:22 -0400

    Mortage Exercise 1.3 - up to 1.10

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,29 @@
-# mortgage.py
-#
-# Exercise 1.7
+
+
+principle = float(input("How much is the mortage:")) 
+mortage_years = int(input("How many years?:"))
+rate = float(input("How much is the rate?:"))
+extra_payment = float(input("How much is the extra payment"))
+start_extra_payment_period = int(input("when do you want to start doing extra payments?"))
+end_extra_payment_period = int(input("when do you want to finsih doing extra payments?"))
+
+count = 1
+totalPaid_after_extraPayment = 0.0
+totalPaid = 0.0
+
+monthly_payment = ((rate/100/12)/(1-(1+rate/100/12)**(-mortage_years*12)))*principle
+
+while principle > 0:
+    if count >= start_extra_payment_period and count <= end_extra_payment_period: 
+            principle = (principle * (1+rate/100/12)) - (monthly_payment+extra_payment)
+            totalPaid_after_extraPayment = totalPaid_after_extraPayment + (monthly_payment+extra_payment)
+    else: 
+            principle = principle * (1+rate/100/12) - monthly_payment
+            totalPaid = totalPaid + monthly_payment
+    
+    print( 'month:', count, ':','total paid:', totalPaid, 'total paid with extra:', totalPaid_after_extraPayment,'remaining principle:', principle)
+    count += 1
+
+totalPayment = totalPaid + totalPaid_after_extraPayment
+
+print('total payment:', totalPayment)
--- Work/sears.py
+++ Work/sears.py
@@ -4,6 +4,7 @@ sears_height   = 442             # Height (meters)
 num_bills      = 1
 day            = 1
 
+
 while num_bills * bill_thickness < sears_height:
     print(day, num_bills, num_bills * bill_thickness)
     day = day + 1
@@ -12,3 +13,12 @@ while num_bills * bill_thickness < sears_height:
 print('Number of days', day)
 print('Number of bills', num_bills)
 print('Final height', num_bills * bill_thickness)
+
+
+
+x = '12.23'
+
+variable = float(x)
+
+
+print(variable)
