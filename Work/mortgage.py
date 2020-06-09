8b3bd1b767f1e62e091b3c7f71a8139ef1328a7d
Maher Bouidani <maher.buedani@gmail.com>
2020-06-09 13:59:12 -0400

    Mortage 1.11 Adding Output Style and Improve Code Readbility

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,4 +1,6 @@
-
+import os
+import importlib
+from style import style
 
 principle = float(input("How much is the mortage:")) 
 mortage_years = int(input("How many years?:"))
@@ -7,11 +9,13 @@ extra_payment = float(input("How much is the extra payment"))
 start_extra_payment_period = int(input("when do you want to start doing extra payments?"))
 end_extra_payment_period = int(input("when do you want to finsih doing extra payments?"))
 
-count = 1
-totalPaid_after_extraPayment = 0.0
-totalPaid = 0.0
+month = 1
+totalRegularPayment = 0.0
+totalExtraPayment = 0.0
 
 monthly_payment = ((rate/100/12)/(1-(1+rate/100/12)**(-mortage_years*12)))*principle
+
+
 
 while principle > 0:
     if principle <= monthly_payment+extra_payment:
@@ -19,16 +23,19 @@ while principle > 0:
             extra_payment = 0.0
             print("There is no need for an extra payment")
 
-    if count >= start_extra_payment_period and count <= end_extra_payment_period:
-            principle = (principle * (1+rate/100/12)) - (monthly_payment+extra_payment)
-            totalPaid_after_extraPayment = totalPaid_after_extraPayment + (monthly_payment+extra_payment)
-    else:                  
-           principle = principle * (1+rate/100/12) - monthly_payment
-           totalPaid = totalPaid + monthly_payment
+    principle = principle * (1+rate/100/12) - monthly_payment
+    totalRegularPayment = totalRegularPayment + monthly_payment
+
+    if month >= start_extra_payment_period and month <= end_extra_payment_period:
+            principle = principle - extra_payment
+            totalExtraPayment = totalExtraPayment + extra_payment           
+           
     
-    print( 'month:', count, ':','total paid:', totalPaid, 'total paid with extra:', totalPaid_after_extraPayment,'remaining principle:', principle)
-    count += 1
+    print( style.WHITE+'month:', month, style.RED+'Total Regular Payment:', totalRegularPayment, style.YELLOW+'Total Extra Payment:', totalExtraPayment, style.BLUE+'remaining principle:', principle)
+    month += 1
 
-totalPayment = totalPaid + totalPaid_after_extraPayment
+totalPayment = totalRegularPayment + totalExtraPayment
+print(style.MAGENTA+'Total Payment:',  totalPayment)
+print(style.RESET)
 
-print('total payment:', totalPayment)
+
