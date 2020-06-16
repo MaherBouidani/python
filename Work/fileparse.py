bcc99ec6c8e921e3c03ac018ce878019aa2d90f1
Maher Bouidani <maher.buedani@gmail.com>
2020-06-16 14:11:56 -0400

    Add a Function 1.33 to Pcost

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,3 +1,7 @@
 # fileparse.py
-#
+def total (a, b):
+    return a * b
+
+
+print(total(3,5))
 # Exercise 3.3
--- Work/pcost.py
+++ Work/pcost.py
@@ -1,13 +1,17 @@
 # pcost.py       
-with open('Data/portfolio.csv', 'rt') as file:
+def portfolio(filename): 
     total = 0.0
-    rowCount = 0
-    for row in file:
-        if rowCount == 0: 
-            print(row)
-        else: 
-            data = row.split(',')
-            total = total + float(data[1]) * float(data[2])
-        rowCount += 1 
-    print("Total:", total)
+    with open(filename, 'rt') as file:
+        rowCount = 0
+        for row in file:
+            if rowCount == 0: 
+                print(row)
+            else: 
+                data = row.split(',')
+                total = total + float(data[1]) * float(data[2])
+            rowCount += 1
+    return total
+
+cost = portfolio('Data/portfolio.csv')
+print("Total:", cost)
 # Exercise 1.27
