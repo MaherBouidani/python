373ec27f4dafdd97c9aa1fd9740acb76a22ed402
Maher Bouidani <maher.buedani@gmail.com>
2020-06-17 03:19:48 -0400

    2.2 Containers Dict as a Container

--- Work/fileProcess.py
+++ Work/fileProcess.py
@@ -1,6 +1,45 @@
-with open('foo.txt', 'wt') as file: 
-        for element in range(0,3):
-            file.write('*')
-        for line in file: 
-            line[0] = line.write('*') 
+
+# Issue - 1 --------------------------
+# import csv 
+
+# file = open('Data/portfolio.csv')
+
+# rows = csv.reader(file)
+
+# def subroutine():
+#     print (next(rows))
+#     if next(rows) != []:
+#        subroutine()
+#     else:
+#         print("The End")
+    
+
+# subroutine()
+#Issue -1 ---------------------------
+
+record = {}
+with open('Data/prices.csv', 'rt') as file: 
+    count = 0
+    for line in file:
+        count += 1 
+    
+    while count > 0: 
+        row = line.split(',')
+        record[row[0]] = float(row[1])
+        print(row)
+
+        count -= 1 
+
+
+
+    
+#     line = 0
+#     while line < 
+#     for line in file:
+    
+#         row = line.split(',')
+#         record[row[0]] = float(row[1])
+
+# print(record)
+
 
